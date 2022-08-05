f22b8e33ee5af95d5b87ab42408d4b5152a461f0
iamanwar94 <iamanwar94ahmed@gmail.com>
2022-08-05 05:52:16 -0700

    05-08-22

---
+++ chatBot/config.js
@@ -0,0 +1,9 @@
+import { createChatBotMessage } from "react-chatbot-kit";
+
+const config = {
+  initialMessages: [
+    createChatBotMessage(`Hello Sir/Madam, How may i help you?`),
+  ],
+};
+
+export default config;
